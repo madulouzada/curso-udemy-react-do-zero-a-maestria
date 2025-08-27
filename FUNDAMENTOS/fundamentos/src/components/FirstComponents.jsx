a97b0791b0d09743028722b4caf14223352a3ca4
maria eduarda louzada <mariaeduardalouzadagirardi@gmail.com>
2025-08-27 07:36:31 -0300

    first commit

---
+++ FUNDAMENTOS/fundamentos/src/components/FirstComponents.jsx
@@ -0,0 +1,10 @@
+const FirstComponents = ()  => { /* função, passando uma variavel */
+
+    return(                       /* retornando algo */                      
+        <div>
+            <h1>Meu primeiro componentes</h1>
+        </div>
+    );
+};
+
+export default FirstComponents /* fazer a exportação, quando desejado */
